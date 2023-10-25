dcc25b0650b2efde5758f68b61b7c0d87308b402
seastamp <seastamp10@gmail.com>
2023-10-25 17:07:05 +0900

    feat: Store 클래스 정의

--- src/model/Store.java
+++ src/model/Store.java
@@ -1,4 +1,9 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Store {
+    public static ArrayList<Menu> menu = new ArrayList<>();
+    public static ArrayList<Order> waintingList = new ArrayList<>(); // 대기중인 주문리스트
+    public static ArrayList<Order> completedList = new ArrayList<>(); // 완료된 주문리스트
 }
