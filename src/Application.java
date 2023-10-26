1b8cfe8dff81b24bb533a4af433af8167f324ad1
JEONGYOUNGGYU <zkdlf90@gmail.com>
2023-10-26 14:36:31 +0900

    전체메뉴,개별메뉴

--- src/Application.java
+++ src/Application.java
@@ -1,3 +1,8 @@
-public class Application {
+import view.CustomerScreen;
 
+public class Application {
+    public static void main(String[] args) {
+        CustomerScreen customerScreen = new CustomerScreen();
+        customerScreen.displayMainMenu();
+    }
 }
--- src/view/CustomerScreen.java
+++ src/view/CustomerScreen.java
@@ -1,14 +1,6 @@
 package view;
 
-import model.Menu;
-import model.Order;
-import model.Product;
-
 public class CustomerScreen {
-    private final static Product SHACKBURGER = new Product("ShackBurger", "토마토, 양상추, 쉑소스가 토핑된 치즈버거", 6.9);
-    private final static Product SMOKESHACK = new Product("SmokeShack", "베이컨, 체리 패퍼에 쉑소스가 토핑된 치즈버거", 6.9);
-    private final static Product SHROOM_BURGER = new Product("ShroomBurger", "몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거", 6.9);
-    private final static Product HAMBURGER = new Product("Hamburger", "비프패티를 기반으로 야채가 들어간 기본버거", 6.9);
 
     public void displayLine(){
         System.out.println("-------------------------------");
@@ -19,10 +11,8 @@ public class CustomerScreen {
     public void displayMainMenu(){
         System.out.println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요");
         System.out.println("[ SHAKESHACK MENU ]");
-        // 메뉴
-
-        System.out.println("[ ORDER MENU ] ");
-        //
+        // 전체 메뉴 불러오기
+        // Order & Cancel 불러오기
     }
     public void displayProductMenu() {
         welcomeMsg();
