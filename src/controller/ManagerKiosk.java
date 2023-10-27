b87109a682eb23f69d99863f086a9e9c3d31fa41
seastamp <seastamp10@gmail.com>
2023-10-27 14:43:34 +0900

    add: 메인메뉴, 대기주문목록, 완료주문목록 handler 구현

--- src/controller/ManagerKiosk.java
+++ src/controller/ManagerKiosk.java
@@ -1,6 +1,11 @@
 package controller;
 
 import input.InputDevice;
+import jdk.jshell.Snippet;
+import model.Menu;
+import model.Order;
+import model.Product;
+import model.Store;
 import view.ManagerScreen;
 
 public class ManagerKiosk extends Kiosk {
@@ -13,6 +18,7 @@ public class ManagerKiosk extends Kiosk {
     private final static int DELETE_PRODUCT = 5;
 
     private static ManagerScreen screen = new ManagerScreen();
+    private static Store store = new Store();
 
     public static void managerKioskStart() {
         while (true) {
@@ -44,21 +50,63 @@ public class ManagerKiosk extends Kiosk {
         }
     }
 
-    public static void handleMainMenu() {
-        switch(InputDevice.receiveInput(4)){
-
+    private static void handleMainMenu() {
+        status = MAIN_MENU;
+        switch (InputDevice.receiveInt(4)) {
+            case 0 -> {
+                status = HOME;
+            }
+            case 1 -> {
+                status = WAITNG_ORDER_LIST;
+            }
+            case 2 -> {
+                status = COMPLETED_ORDER_LIST;
+            }
+            case 3 -> {
+                status = CREAT_PRODUCT;
+            }
+            case 4 -> {
+                status = DELETE_PRODUCT;
+            }
         }
     }
-    public static void handleWaitingOrderMenu() {
+
+    private static void handleWaitingOrderMenu() {
+        int selectedNumber = InputDevice.receiveInt(Store.waitingList.size());
+        int answer;
+        if (selectedNumber == 0) {
+            status = MAIN_MENU;
+        } else if (selectedNumber != -1) {
+            do {
+                // 주문 완료 처리하시겠습니까 메서드에 (Store.waitingList.get(answer-1))
+                // 출력을 한다면
+                answer = InputDevice.receiveInt(2);
+            } while (answer == -1);
+            status = MAIN_MENU;
+            if (answer == 1) {
+                store.changeCompleteOrderState(Store.waitingList.get(selectedNumber - 1));
+            }
+        }
+    }
+
+    private static void handleCompletedOrderMenu() {
+        int selectedNumber = InputDevice.receiveInt(Store.completedList.size());
+        int answer;
+        if (selectedNumber == 0) {
+            status = MAIN_MENU;
+        } else if (selectedNumber != -1) {
+                // 완료 메뉴 출력(Store.waitingList.get(answer-1))
+                // ! 0에 대한 에러처리가 안되어있음.
+            // 5초후 화면 전환이 더 자연스러울듯
+            status = MAIN_MENU;
+        }
+    }
+
+    private static void handleCreateProduct() {
 
     }
-    public static void handleCompletedOrderMenu() {
 
-    }
-    public static void handleCreateProduct() {
-
-    }
-    public static void handleDeleteProduct() {
+    private static void handleDeleteProduct() {
 
     }
 
