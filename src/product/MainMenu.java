786b6ec887f3e1fde2e61cb9e45f764b87b5d160
JEONGYOUNGGYU <zkdlf90@gmail.com>
2023-10-27 09:51:24 +0900

    mainMenu/ShakeShackAAllMenu 수정

--- src/product/MainMenu.java
+++ src/product/MainMenu.java
@@ -6,6 +6,7 @@ import model.Product;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainMenu {
     public final static Menu BURGERS = new Menu("Burgers", "앵거스 비프통살을 다져만든 버거");
@@ -13,6 +14,6 @@ public class MainMenu {
     public final static Menu DRINKS = new Menu("Drinks", "매장에서 직접 만드는 음료");
     public final static Menu CONCRETES = new Menu("Concretes", "쫀득하고 진한맛의 커스터드");
 
-   public ArrayList<Menu> mainMenu = new ArrayList<>(Arrays.asList(BURGERS, CUSTARDS, DRINKS, CONCRETES));
+   public List<Menu> mainMenu = new ArrayList<>(Arrays.asList(BURGERS, CUSTARDS, DRINKS, CONCRETES));
 
 }
