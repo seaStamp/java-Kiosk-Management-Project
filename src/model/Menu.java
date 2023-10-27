7d4e2d67dbad2648cc4f1171f2e4f01d8796c83a
minahYu <mina010311@naver.com>
2023-10-27 11:26:01 +0900

    Order & Store

--- src/model/Menu.java
+++ src/model/Menu.java
@@ -7,10 +7,7 @@ public class Menu {
     protected int id = 0;
     protected String name;
     protected String info;
-<<<<<<< HEAD
     //private ArrayList<Product> products; // 각 메뉴에 해당하는 상품들을 담는 리스트
-=======
->>>>>>> f9cf34a72bcf320f0f1398389b390ed9a53b015d
 
     // 기본 생성자
     public Menu() {
